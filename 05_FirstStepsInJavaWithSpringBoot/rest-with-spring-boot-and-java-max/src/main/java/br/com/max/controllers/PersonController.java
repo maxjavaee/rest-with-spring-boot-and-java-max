fd576ef39ab4a1e9c9855fe885fe77bf4c8def50
Max <maxvzp@yahoo.com.br>
2024-01-29 10:02:49 -0300

    Atualizando

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-max/src/main/java/br/com/max/controllers/PersonController.java
@@ -0,0 +1,58 @@
+package br.com.max.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.max.model.Person;
+import br.com.max.services.PersonServices;
+
+@RestController
+@RequestMapping(value = "/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	
+	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+ 
+		return service.create(person);
+
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+
+		return service.update(person);
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) throws Exception {
+
+		return service.findById(id);
+
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable(value = "id") String id) throws Exception {
+
+		service.delete(id);
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+
+		return service.findAll();
+
+	}
+
+}
