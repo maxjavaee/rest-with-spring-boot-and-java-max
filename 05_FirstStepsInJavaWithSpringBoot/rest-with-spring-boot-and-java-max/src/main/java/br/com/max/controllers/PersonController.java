fc8af0a323eef776b7efd76e4bbb0eab6442970a
Max <maxvzp@yahoo.com.br>
2024-01-29 10:13:38 -0300

    Novo teste commit

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-max/src/main/java/br/com/max/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-max/src/main/java/br/com/max/controllers/PersonController.java
@@ -19,14 +19,14 @@ public class PersonController {
 
 	@Autowired
 	private PersonServices service;
-	
+
 	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Person create(@RequestBody Person person) {
- 
+
 		return service.create(person);
 
 	}
-	
+
 	@RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Person update(@RequestBody Person person) {
 
@@ -40,7 +40,7 @@ public class PersonController {
 		return service.findById(id);
 
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public void delete(@PathVariable(value = "id") String id) throws Exception {
 
